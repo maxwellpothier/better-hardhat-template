f4b8780ebc201040318c2c27bb72085371c84eae
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-03 22:44:01 -0600

    finishes deploy script. Live on Goerli

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Fork this repo for a more minimal, practical Hardhat template to build from
 
 My purpose in creating this project is to solidify my understanding of the Hardhat framework and make a template for myself and others that is a bit more accessible with less overhead.
+
+Result is a verified contract on the goerli testnet, though the code is written to be able to plug in any network to deploy to. Here's an example: https://goerli.etherscan.io/address/0x7E8B6eFE28D504020BFc59ccee3f1506A5b3CF32#code
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,5 +1,13 @@
 const hre = require("hardhat");
 
+const sleep = async (ms) => {
+	return new Promise((res) => {
+		setTimeout(() => {
+			res();
+		}, ms);
+	});
+}
+
 const main = async () => {
 	const HelloWorld = await hre.ethers.getContractFactory("HelloWorld");
 	const helloWorld = await HelloWorld.deploy("Initial message");
@@ -7,6 +15,14 @@ const main = async () => {
 	await helloWorld.deployed();
 
 	console.log(`HelloWorld contract deployed to ${helloWorld.address}`);
+
+	// Delay the Etherscan verification
+	await sleep(30 * 1000);
+
+	await hre.run("verify:verify", {
+		address: helloWorld.address,
+		constructorArguments: ["Initial message"],
+	});
 }
 
 main().catch((error) => {
