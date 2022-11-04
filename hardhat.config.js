f593063c1351f7a569760a80cf8725fe854e068f
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-03 22:31:10 -0600

    deploy script and config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: "0.8.17",
+	networks: {
+		goerli: {
+			url: process.env.RPC_URL,
+			accounts: [process.env.PRIVATE_KEY],
+		},
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_KEY,
+	}
 };
